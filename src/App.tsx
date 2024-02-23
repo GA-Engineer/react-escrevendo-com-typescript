d2c91a1bb380f8f0ac97a71aaee2a1a82ba9abc2
Gabriella Andrade <gabriellaandrade.d@hotmail.com>
2024-02-22 22:23:28 -0300

    feat: creating firsts components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Formulario from './components/formulario';
+import Lista from './components/lista';
+
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Formulario/>
+      <Lista/>
     </div>
   );
 }
